3c5944ebd3927045f43598c5da68f887c7f0a106
Dimitri Kurashvili <dimakura@gmail.com>
2017-09-23 20:27:04 +0400

    Add scripts for starting and stopping instances (#2)

--- scripts/instance.sh
+++ scripts/instance.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+source ./scripts/config.sh
+source ./scripts/firewall.sh
+
 # This returns status of the instance. Possible values for status are:
 #
 # - (empty response) -- there is no such instance
@@ -12,8 +15,44 @@ instance_status() {
   gcloud compute instances describe $1 | grep "^status:*" | cut -d' ' -f 2
 }
 
+start_instance() {
+  local instance_name=$1
+  local status=$(instance_status $instance_name)
+  if [ $status == 'TERMINATED' ]; then
+    echo "Starting $instance_name..."
+    gcloud compute instances start $instance_name
+    instance_status $instance_name
+  else
+    echo "Instance $instance_name is already started (status=$status)."
+    exit 1
+  fi
+}
+
+stop_instance() {
+  local instance_name=$1
+  local status=$(instance_status $instance_name)
+  if [ $status == 'RUNNING' ]; then
+    echo "Stopping $instance_name..."
+    gcloud compute instances stop $instance_name
+    instance_status $instance_name
+  else
+    echo "Instance $instance_name is already stop (status=$status)."
+    exit 1
+  fi
+}
+
+# CPU instance functions
+
+cpu_instance_status() {
+  instance_status $CPU_INSTANCE_NAME
+}
+
+gpu_instance_status() {
+  instance_status $GPU_INSTANCE_NAME
+}
+
 create_cpu_instance() {
-  local status=$(instance_status $CPU_INSTANCE_NAME)
+  local status=$(cpu_instance_status)
 
   if [ -z $status ]; then
     echo "Creating $CPU_INSTANCE_NAME..."
@@ -32,7 +71,18 @@ create_cpu_instance() {
            --boot-disk-size $CPU_DISK_SIZE\
            --boot-disk-type "pd-standard"\
            --boot-disk-device-name $CPU_INSTANCE_NAME
+    allow_http
+    allow_https
   else
     echo "Instance $CPU_INSTANCE_NAME already exists and has status $status"
+    exit 1
   fi
+}
+
+start_cpu_instance() {
+  start_instance $CPU_INSTANCE_NAME
+}
+
+stop_cpu_instance() {
+  stop_instance $CPU_INSTANCE_NAME
 }
