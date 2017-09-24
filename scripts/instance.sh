5f7c08c8364532d6cc6906e765325322cd0c1dbd
Dimitri Kurashvili <dimakura@gmail.com>
2017-09-24 21:33:39 +0400

    Cleanup: rely on default params, prevent some unwanted behaviour

--- scripts/instance.sh
+++ scripts/instance.sh
@@ -75,14 +75,11 @@ create_cpu_instance() {
 
   if [ -z $status ]; then
     echo "Creating $CPU_INSTANCE_NAME..."
-    gcloud beta compute --project $PROJECT\
-           instances create $CPU_INSTANCE_NAME\
-           --zone $ZONE\
+    gcloud beta compute instances create $CPU_INSTANCE_NAME\
            --machine-type $CPU_MACHINE_TYPE\
            --subnet "default"\
            --maintenance-policy "MIGRATE"\
-           --service-account $SERVICE_ACCOUNT\
-           --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring.write","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append"\
+           --scopes $CPU_SERVICE_SCOPES
            --min-cpu-platform "Automatic"\
            --tags $HTTP_SERVER,$HTTPS_SERVER\
            --image $IMAGE\
