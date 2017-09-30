f1d26d7b74fd0eb7b49e6e85cd81be89c9667fa8
Dimitri Kurashvili <dimakura@gmail.com>
2017-09-30 21:56:19 +0400

    Use 2 CPUs, as required by google

--- scripts/config.sh
+++ scripts/config.sh
@@ -22,14 +22,14 @@ readonly HTTPS_SERVER="https-server"
 # CPU instance settings
 readonly CPU_INSTANCE_NAME="fastai-cpu-instance"
 readonly CPU_DISK_SIZE="32"
-readonly CPU_MACHINE_TYPE="custom-1-8192"
+readonly CPU_MACHINE_TYPE="custom-2-8192"
 readonly CPU_SERVICE_SCOPES=("https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring.write","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append")
 readonly CPU_MAINTENANCE_POLICY="MIGRATE"
 
 # GPU instance settings
 readonly GPU_INSTANCE_NAME="fastai-gpu-instance"
 readonly GPU_DISK_SIZE="48"
-readonly GPU_MACHINE_TYPE="custom-1-8192"
+readonly GPU_MACHINE_TYPE="custom-2-8192"
 readonly GPU_SERVICE_SCOPES=$CPU_SERVICE_SCOPES
 readonly GPU_MAINTENANCE_POLICY="TERMINATE"
 readonly GPU_CARD_TYPE="nvidia-tesla-k80"
