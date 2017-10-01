5cece30332027459f6b1cc54633202f8164bb888
Dimitri Kurashvili <dimakura@gmail.com>
2017-10-01 19:51:12 +0400

    Update NVIDIA driver

--- scripts/install_ubuntu.sh
+++ scripts/install_ubuntu.sh
@@ -10,9 +10,8 @@ sudo apt-get --assume-yes install software-properties-common
 
 # download and install GPU drivers
 if [ ${gpu} = 1 ]; then
-  wget "http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.44-1_amd64.deb" -O "cuda-repo-ubuntu1604_8.0.44-1_amd64.deb"
-
-  sudo dpkg -i cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
+  wget "http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb" -O "cuda-repo-ubuntu1604_9.0.176-1_amd64.deb"
+  sudo dpkg -i cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
   sudo apt-get update
   sudo apt-get -y install cuda
   sudo modprobe nvidia
@@ -72,7 +71,8 @@ c.NotebookApp.open_browser = False" >> $HOME/.jupyter/jupyter_notebook_config.py
 
 # clone the fast.ai course repo
 cd ~
-ssh-keygen -t rsa -b 4096 -f .ssh/id_rsa -q -C "dimakura@gmail.com" -P ""
+# ssh-keygen -t rsa -b 4096 -f .ssh/id_rsa -q -C "dimakura@gmail.com" -P ""
 git clone https://github.com/dimakura/fast.ai.git
-cd fast.ai/
+cd ~/fast.ai/
 git fetch --all
+mkdir ~/fast.ai/data
